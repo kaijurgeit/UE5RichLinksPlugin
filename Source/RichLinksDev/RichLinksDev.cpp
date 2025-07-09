0350f90220f562c697e5de87c0e70104565f5c5d
Kai Jurgeit <kai.jurgeit@t-online.de>
2025-07-09 22:10:51 +0200

    chore: UE5.3 project create (blank, c++)

---
+++ Source/RichLinksDev/RichLinksDev.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RichLinksDev.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RichLinksDev, "RichLinksDev" );
